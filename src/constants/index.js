3abd6f98e62bb87e852f5c4f337314b83d24698c
IhebMth <ihebmeftah@outlook.fr>
2025-01-05 12:54:16 +0100

    'quiz-app'

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,7 @@
 import  winProd from "../assets/images/winProd.png"
 import commersaDigital from '../assets/images/commersaDigital.png'
+import yanogo from '../assets/images/yanogo.png'
+
 import {
     car,
     contact,
@@ -27,6 +29,7 @@ import {
 import wedding from '../assets/icons/wedding.svg'
 import bootstrap from '../assets/icons/bootstrap.svg'
 import movies from '../assets/icons/movies.png'
+import quizApp from '../assets/icons/quizLogo.png'
 
 
 export const skills = [
@@ -106,13 +109,26 @@ export const skills = [
 
 export const experiences = [
     {
+        title: "Frontend Developer  (React.js / Node.js / Jira / GitLab)",
+        company_name: "Yanogo",
+        icon: yanogo,
+        iconBg: "#accbe1",
+        date: "06 / 04 / 2023 - 06/06/2024",
+        points: [
+            " Web Design & User Interface: Developed user-friendly and responsive interfaces using React.js. I implemented key components that enhanced the overall user experience, ensuring that the platform was intuitive and easy to navigate for job seekers optimizing their CVs. I applied CSS and Material-UI to create visually consistent and interactive pages",
+          " Backend API Integration: Integrated backend APIs into the front end to enable the platform’s AI-driven CV optimization system. I managed API calls and data handling to display real-time suggestions based on user inputs. This involved ensuring smooth interaction between the front-end and backend systems, allowing for seamless data flow and user interactions.",
+           " Collaboration & Task Management: Actively participated in team sprints and task management using Jira. I regularly updated tasks, tracked progress, and worked closely with other developers to ensure features were delivered on time. I contributed to the completion of assigned tasks within the sprint cycles, focusing on bug fixes and feature improvements"
+        ],
+    },
+
+    {
         title: "Web Designer Freelancer",
         company_name: "Win Prod",
         icon: winProd,
         iconBg: "#accbe1",
         date: "17/07/2023 - Current",
         points: [
-            " Creating posters/ Graphic Charts / Logos",
+            " Creating posters/ Graphic Charts ",
             "Assisting in web development Projects and Bringing New Ideas",
             "Content creation and optimization / Keeping up with search engine algorithms",
             "Direct contact with clients when needed"
@@ -179,6 +195,14 @@ export const projects = [
         description: "i created an movies app where i did put all my learning skills into action.. i have used reactJs, hooks, tdmp Api, axios ,  scss, Implementing a video player that reads all kind of sources that i find it a solution after alot of research, ads  using admob , it will be live in february  ",
         githubLink: 'https://github.com/IhebMth/ArabFlix',
         liveLink : 'https://arabflix.netlify.app/'
+    },
+    {
+        iconUrl: quizApp,
+        theme: 'btn-back-black',
+        name: 'QuizApp',
+        description: "Built an interactive quiz app using React and Tailwind CSS, featuring drag-and-drop functionality, real-time stats, and timed quizzes. Focused on English and Mathematics, the app delivers engaging learning experiences with responsive design and final result summaries, making education fun and interactive for students of all levels",
+        githubLink: 'https://github.com/IhebMth/Quiz-App',
+        liveLink : 'https://quiz-appixl.vercel.app/'
     },
     {
         iconUrl: threads,
